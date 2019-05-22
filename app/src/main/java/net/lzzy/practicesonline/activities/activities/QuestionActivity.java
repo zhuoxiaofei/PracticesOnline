4062ad4c88c8bad1b271233c5fcb23dd616e247a
Administrator <895885871@qq.com>
2019-05-22 12:08:07 +0800

    5.22

--- app/src/main/java/net/lzzy/practicesonline/activities/activities/QuestionActivity.java
+++ app/src/main/java/net/lzzy/practicesonline/activities/activities/QuestionActivity.java
@@ -11,6 +11,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import net.lzzy.practicesonline.R;
 import net.lzzy.practicesonline.activities.fragments.QuestionFragment;
+import net.lzzy.practicesonline.activities.models.FavoriteFactory;
 import net.lzzy.practicesonline.activities.models.Question;
 import net.lzzy.practicesonline.activities.models.QuestionFactory;
 import net.lzzy.practicesonline.activities.models.UserCookies;
@@ -37,7 +39,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static net.lzzy.practicesonline.activities.activities.ResultActivity.POSITION;
+import static net.lzzy.practicesonline.activities.activities.ResultActivity.QUESTION;
 import static net.lzzy.practicesonline.activities.activities.ResultActivity.RESULT_CODE;
+import static net.lzzy.practicesonline.activities.activities.ResultActivity.RESULT_CODE_TWO;
 
 /**
  * @author Administrator
@@ -46,6 +50,7 @@ public class QuestionActivity extends AppCompatActivity {
     private static final int WHAT_PRACTICE_DONE = 0;
     private static final int WHAT_EXCEPTION = 1;
     public static final int EXTRA_REQUEST_CODE = 0;
+    public static final int EXTRA_RESULT_CODE = 5;
 
     private String practiceId;
     private int apiId;
@@ -53,18 +58,18 @@ public class QuestionActivity extends AppCompatActivity {
     private TextView tvView;
     private TextView tvCommit;
     private ViewPager pager;
-    private boolean isCommitted=false;
+    private boolean isCommitted = false;
     private TextView tvHint;
     private int pos;
     private LinearLayout container;
     private View[] dost;
-    private DownloadHandler handler= new DownloadHandler(this);
-    public static final String EXTRA_PRACTICE_ID= "practiceId";
-    public static final String EXTRA_RESULTS= "results";
+    private DownloadHandler handler = new DownloadHandler(this);
+    public static final String EXTRA_PRACTICE_ID = "practiceId";
+    public static final String EXTRA_RESULTS = "results";
+    private FragmentStatePagerAdapter adapter;
 
     /**
      * 自定义线程 返回数据的方法
-     *
      */
     private static class DownloadHandler extends AbstractStaticHandler<QuestionActivity> {
 
@@ -74,19 +79,19 @@ public class QuestionActivity extends AppCompatActivity {
 
         @Override
         public void handleMessage(Message msg, QuestionActivity questionActivity) {
-            switch (msg.what){
+            switch (msg.what) {
                 case WHAT_PRACTICE_DONE:
                     ViewUtils.dismissProgress();
-                    questionActivity.isCommitted=true;
+                    questionActivity.isCommitted = true;
                     Toast.makeText(questionActivity, "提交成功", Toast.LENGTH_SHORT).show();
                     UserCookies.getInstance().commitPractice(questionActivity.practiceId);
                     questionActivity.redirect();
                     break;
                 case WHAT_EXCEPTION:
-                    Toast.makeText(questionActivity,"提交失败，请重试",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(questionActivity, "提交失败，请重试", Toast.LENGTH_SHORT).show();
                     break;
-                    default:
-                        break;
+                default:
+                    break;
             }
         }
     }
@@ -118,7 +123,7 @@ public class QuestionActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 refreshDots(position);
-                UserCookies.getInstance().updateCurrentQuestion(practiceId,position);
+                UserCookies.getInstance().updateCurrentQuestion(practiceId, position);
                 UserCookies.getInstance().updateReadCount(questions.get(position).getId().toString());
             }
 
@@ -130,88 +135,121 @@ public class QuestionActivity extends AppCompatActivity {
         tvCommit.setOnClickListener(v -> commitPractice());
         tvView.setOnClickListener(v -> redirect());
     }
-    /**查看成绩*/
+
+    /**
+     * 查看成绩
+     */
     private void redirect() {
-        List<QuestionResult> results= UserCookies.getInstance().getResultFromCoolies(questions);
-        Intent intent=new Intent(this, ResultActivity.class);
-        intent.putExtra(EXTRA_PRACTICE_ID,practiceId);
+        List<QuestionResult> results = UserCookies.getInstance().getResultFromCoolies(questions);
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(EXTRA_PRACTICE_ID, practiceId);
         intent.putParcelableArrayListExtra(EXTRA_RESULTS, (ArrayList<? extends Parcelable>) results);
         startActivityForResult(intent, EXTRA_REQUEST_CODE);
     }
 
-    /**返回处理*/
+    /**
+     * 返回处理
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==EXTRA_REQUEST_CODE&&resultCode== RESULT_CODE){
-            int position=data.getIntExtra(POSITION,-1);
+        if (requestCode == EXTRA_REQUEST_CODE && resultCode == RESULT_CODE) {
+            int position = data.getIntExtra(POSITION, -1);
             pager.setCurrentItem(position);
         }
+        //查看收藏
+        if (requestCode == EXTRA_REQUEST_CODE && resultCode == RESULT_CODE_TWO) {
+            String result = data.getStringExtra(QUESTION);
+            if (!result.isEmpty()) {
+                List<Question> questionList = new ArrayList<>();
+                FavoriteFactory factory = FavoriteFactory.getInstance();
+                for (Question q : QuestionFactory.getInstance().getByPractices(result)) {
+                    if (factory.isQuestionStarred(q.getId().toString())) {
+                        questionList.add(q);
+                    }
+                }
+                //清空
+                questions.clear();
+                //重新加载
+                questions.addAll(questionList);
+                initDots();
+                adapter.notifyDataSetChanged();
+                if (questions.size() > 0) {
+                    pager.setCurrentItem(0);
+                    refreshDots(0);
+                }
+            }
+
+        }
+
 
     }
 
     String info;
-    /**选择mac地址*/
+
+    /**
+     * 选择mac地址
+     */
     private void commitPractice() {
-        List<QuestionResult> results= UserCookies.getInstance().getResultFromCoolies(questions);
-        List<String> macs= AppUtils.getMacAddress();
+        List<QuestionResult> results = UserCookies.getInstance().getResultFromCoolies(questions);
+        List<String> macs = AppUtils.getMacAddress();
         String[] items = new String[macs.size()];
         macs.toArray(items);
         info = items[0];
         new AlertDialog.Builder(this)
                 .setTitle("选择mac地址")
                 //单选setSingleChoiceItems，， //多选setMultiChoiceItems
-                .setSingleChoiceItems(items,0,(dialog, which) -> info = items[which])
-                .setNeutralButton("取消",null)
-                .setPositiveButton("提交",(dialog, which) -> {
-                    PracticeReult result = new PracticeReult(results,apiId,"卓宵飞,"+info);
+                .setSingleChoiceItems(items, 0, (dialog, which) -> info = items[which])
+                .setNeutralButton("取消", null)
+                .setPositiveButton("提交", (dialog, which) -> {
+                    PracticeReult result = new PracticeReult(results, apiId, "卓宵飞," + info);
                     //提交方法
                     postResult(result);
                 }).show();
     }
 
     private void postResult(PracticeReult result) {
-        ViewUtils.showProgress(this,"正在提交成绩");
-            AppUtils.getExecutor().execute(()->{
-                try {
-                    int aa= PracticeService.postResult(result);
-                    if (aa>=200 && aa<=220){
-                        handler.sendMessage(handler.obtainMessage(WHAT_PRACTICE_DONE,aa));
-                    }
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
-                    handler.sendMessage(handler.obtainMessage(WHAT_EXCEPTION,e.getMessage()));
+        ViewUtils.showProgress(this, "正在提交成绩");
+        AppUtils.getExecutor().execute(() -> {
+            try {
+                int aa = PracticeService.postResult(result);
+                if (aa >= 200 && aa <= 220) {
+                    handler.sendMessage(handler.obtainMessage(WHAT_PRACTICE_DONE, aa));
                 }
-            });
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+                handler.sendMessage(handler.obtainMessage(WHAT_EXCEPTION, e.getMessage()));
+            }
+        });
     }
 
     private void initDots() {
         //region  导航栏
-        int count=questions.size();
+        int count = questions.size();
         dost = new View[count];
         container = findViewById(R.id.activity_question_dots);
         container.removeAllViews();
-        int px= ViewUtils.dp2px(16,this);
+        int px = ViewUtils.dp2px(16, this);
 
-        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(px,px);
-        px= ViewUtils.dp2px(5,this);
-        params.setMargins(px,px,px,px);
-        for (int i=0;i<count;i++){
-            TextView tvDot=new TextView(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(px, px);
+        px = ViewUtils.dp2px(5, this);
+        params.setMargins(px, px, px, px);
+        for (int i = 0; i < count; i++) {
+            TextView tvDot = new TextView(this);
             tvDot.setLayoutParams(params);
             tvDot.setBackgroundResource(R.drawable.dot_style);
             tvDot.setTag(i);
             //todo:tvDot添加点击监听
             tvDot.setOnClickListener(v -> pager.setCurrentItem((Integer) v.getTag()));
             container.addView(tvDot);
-            dost[i]=tvDot;
+            dost[i] = tvDot;
         }
         //endregion
     }
 
-    private void refreshDots(int pos){
-        for (int i= 0;i<dost.length;i++){
-            int drawable=i==pos? R.drawable.dot_fill_style: R.drawable.dot_style;
+    private void refreshDots(int pos) {
+        for (int i = 0; i < dost.length; i++) {
+            int drawable = i == pos ? R.drawable.dot_fill_style : R.drawable.dot_style;
             dost[i].setBackgroundResource(drawable);
         }
     }
@@ -221,25 +259,42 @@ public class QuestionActivity extends AppCompatActivity {
         tvCommit = findViewById(R.id.activity_question_tv_commit);
         tvHint = findViewById(R.id.activity_question_tv_hint);
         pager = findViewById(R.id.activity_question_pager);
-        if (isCommitted){
+        if (isCommitted) {
             tvCommit.setVisibility(View.GONE);
             tvView.setVisibility(View.VISIBLE);
             tvHint.setVisibility(View.VISIBLE);
-        }else {
+        } else {
             tvCommit.setVisibility(View.VISIBLE);
             tvView.setVisibility(View.GONE);
             tvHint.setVisibility(View.GONE);
         }
-        FragmentStatePagerAdapter adapter=new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+        adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            int count = 0;
+
             @Override
             public Fragment getItem(int position) {
-                Question question=questions.get(position);
-                return QuestionFragment.newInstance(question.getId().toString(),position,isCommitted);
+                Question question = questions.get(position);
+                return QuestionFragment.newInstance(question.getId().toString(), position, isCommitted);
             }
 
             @Override
             public int getCount() {
                 return questions.size();
+            }
+
+            @Override
+            public void notifyDataSetChanged() {
+                count = getCount();
+                super.notifyDataSetChanged();
+            }
+
+            @Override
+            public int getItemPosition(@NonNull Object object) {
+                if (count > 0) {
+                    count--;
+                    return POSITION_NONE;
+                }
+                return super.getItemPosition(object);
             }
         };
         pager.setAdapter(adapter);
@@ -247,10 +302,10 @@ public class QuestionActivity extends AppCompatActivity {
 
     private void retrieveDate() {
         practiceId = getIntent().getStringExtra(PracticesActivity.EXTRA_PRACTICE_ID);
-        apiId = getIntent().getIntExtra(PracticesActivity.EXTRA_API_ID,-1);
-        questions= QuestionFactory.getInstance().getByPractices(practiceId);
-        isCommitted= UserCookies.getInstance().isPracticeCommitted(practiceId);
-        if (apiId<0||questions==null||questions.size()==0){
+        apiId = getIntent().getIntExtra(PracticesActivity.EXTRA_API_ID, -1);
+        questions = QuestionFactory.getInstance().getByPractices(practiceId);
+        isCommitted = UserCookies.getInstance().isPracticeCommitted(practiceId);
+        if (apiId < 0 || questions == null || questions.size() == 0) {
             Toast.makeText(this, "no questions", Toast.LENGTH_SHORT).show();
             finish();
         }
--- app/src/main/java/net/lzzy/practicesonline/activities/activities/ResultActivity.java
+++ app/src/main/java/net/lzzy/practicesonline/activities/activities/ResultActivity.java
@@ -1,7 +1,10 @@
 package net.lzzy.practicesonline.activities.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import net.lzzy.practicesonline.R;
@@ -12,17 +15,25 @@ import net.lzzy.practicesonline.activities.models.view.QuestionResult;
 import java.util.List;
 
 /**
- *
  * @author lzzy_gxy
  * @date 2019/5/13
  * Description:
  */
-public class ResultActivity extends BaseActivity implements GridFragment.OnGridListener ,
-GridFragment.GetChartFragmentListener,ChartFragment.GetGridFragmentListener {
+public class ResultActivity extends BaseActivity implements GridFragment.OnGridListener,
+        GridFragment.GetChartFragmentListener, ChartFragment.GetGridFragmentListener {
 
     public static final String POSITION = "position";
     public static final int RESULT_CODE = 1;
+    public static final String QUESTION = "question";
+    public static final int RESULT_CODE_TWO = 2;
     private List<QuestionResult> results;
+    private String question;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        question = getIntent().getStringExtra(QuestionActivity.EXTRA_PRACTICE_ID);
+    }
 
     @Override
     protected int getLayoutRes() {
@@ -39,24 +50,69 @@ GridFragment.GetChartFragmentListener,ChartFragment.GetGridFragmentListener {
         results = getIntent().getParcelableArrayListExtra(QuestionActivity.EXTRA_RESULTS);
         return GridFragment.newInstance(results);
     }
-/**点击序号跳转到相对应的题目*/
+
+    /**
+     * 点击序号跳转到相对应的题目
+     */
     @Override
     public void onGrid(int position) {
-        Intent intent=new Intent();
-        intent.putExtra(POSITION,position);
-        setResult(RESULT_CODE,intent);
+        Intent intent = new Intent();
+        intent.putExtra(POSITION, position);
+        setResult(RESULT_CODE, intent);
         finish();
     }
-/**点击右下角的"图"跳转到ChartFragment图形视图*/
+
+    /**
+     * 点击右下角的"图"跳转到ChartFragment图形视图
+     */
     @Override
     public void getChartFragment() {
         getManager().beginTransaction().replace(R.id.activity_result_container,
                 ChartFragment.newInstance(results)).commit();
     }
-    /**点击右下角的"表"跳转到GridFragment表型视图*/
+
+    /**
+     * 点击右下角的"表"跳转到GridFragment表型视图
+     */
     @Override
     public void getGridFragment() {
         getManager().beginTransaction().replace(R.id.activity_result_container,
                 GridFragment.newInstance(results)).commit();
+    }
+
+    /**
+     * 返回
+     */
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("返回到哪里？")
+                .setNeutralButton("返回题目", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(ResultActivity.this, QuestionActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                })
+                .setNegativeButton("章节列表", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(ResultActivity.this, PracticesActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .setPositiveButton("查看收藏", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent();
+                        intent.putExtra(QUESTION, question);
+                        setResult(RESULT_CODE_TWO, intent);
+                        finish();
+                    }
+                })
+                .show();
     }
 }
